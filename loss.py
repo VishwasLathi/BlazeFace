4c378bd3ed6f63f755008732b7fa1233b54181f7
VishwasLathi <lathivishwas@gmail.com>
2020-04-26 15:25:07 +0530

    made changes to the loss function

--- loss.py
+++ loss.py
@@ -17,6 +17,8 @@ def smooth_l1(x):
 #second dimentions (320) corresponds to the total feature points (16*16 + 8*8) for to the two feature maps of sizes 16*16 and 8*8. 
 def smooth_l1_loss(true, pred):
 
+
+
     face_label = true[:, :, :1]
     #target center x coordinate. 
     gxs = true[:, :, 1:2]
@@ -27,20 +29,25 @@ def smooth_l1_loss(true, pred):
     #target height
     ghs = true[:, :, 4:5]
 
+    dxs = true[:, :, 5:6]
+    dys = true[:, :, 6:7]
+    dws = true[:, :, 7:8]
+    dhs = true[:, : ,8:9]
+
     pxs = pred[:, :, 1:2]
     pys = pred[:, :, 2:3]
     pws = pred[:, :, 3:4]
     phs = pred[:, :, 4:5]
 
-    logitx = (gxs - pxs) / pws
-    logity = (gys - pys) / phs
-    logitw = tf.math.log(gws / pws)
-    logith = tf.math.log(ghs / phs)
+    logitx = (gxs - dxs) / dws
+    logity = (gys - dys) / dhs
+    logitw = tf.math.log(gws / dws)
+    logith = tf.math.log(ghs/ dhs)
 
-    lossx = face_label * tf.map_fn(smooth_l1, tf.reshape(gxs - logitx, (-1, 320)))
-    lossy = face_label * tf.map_fn(smooth_l1, tf.reshape(gys - logity, (-1, 320)))
-    lossw = face_label * tf.map_fn(smooth_l1, tf.reshape(gws - logitw, (-1, 320)))
-    lossh = face_label * tf.map_fn(smooth_l1, tf.reshape(ghs - logith, (-1, 320)))
+    lossx = face_label * tf.map_fn(smooth_l1, tf.reshape(pxs - logitx, (-1, 320)))
+    lossy = face_label * tf.map_fn(smooth_l1, tf.reshape(pys - logity, (-1, 320)))
+    lossw = face_label * tf.map_fn(smooth_l1, tf.reshape(pws - logitw, (-1, 320)))
+    lossh = face_label * tf.map_fn(smooth_l1, tf.reshape(phs - logith, (-1, 320)))
 
     x_sum = tf.reduce_sum(lossx)
     y_sum = tf.reduce_sum(lossy)
--- utils.py
+++ utils.py
@@ -20,25 +20,26 @@ def get_bounding_boxes(annotation):
 #loads the dataset in the form of : images,[scores,scores_and_coordinates]
 #returns randomly generated data, for the purpose of testing the training loop.
 def dataloader(path_to_dataset = ''):
-    return np.float32(np.random.random((64, 128, 128, 3))), [np.float32(np.random.random((64,320,1))),np.float32(np.random.random((64,320,5)))]
-
+    return np.float32(np.random.random((64, 128, 128, 3))), [np.float32(np.random.random((64,320,1))),np.float32(np.random.random((64,320,9)))]
 #For the purpose of simplicity, currently we have 1 anchor box (aspect ratio 1:1) for each point in the 8*8 and 16*16 feaature map.
 #maps the anchors to the original image (128*128*3).
 #will be called by dataloader to provide the target scores and coordinates for each anchor. 
 def map_anchors(path_to_annotations):
-    annotations = os.listdir(path_to_annotations)
+    # annotations = os.listdir(path_to_annotations)
     batch_score_8 = []
     batch_loc_8 = []
     batch_score_16 = []
     batch_loc_16 =  []
+    annotations = [1,2,3,4]
     for annotation in annotations:  
         #boxes : [x_left,y_top, width, height]
-        boxes = get_bounding_boxes(annotation)
+        # boxes = get_bounding_boxes(annotation)
+        boxes = []
         scores_16 = []
         loc_16 = []
         threshold = 0.5
         for x in range(0,128,8):
-            for y in range(0,128,8):
+            for y in range(128,0,-8):
                 iou = 0
                 box = []
                 for b in boxes:
@@ -48,15 +49,15 @@ def map_anchors(path_to_annotations):
                         box = b
                 if iou > threshold:
                     scores_16.append(1)
-                    loc_16.append([1, (box[0]+box[2]/2)/128,(box[1]-box[3]/2)/128,box[2]/128,box[3]/128])
+                    loc_16.append([1,(box[0]+box[2]/2)/128,(box[1]-box[3]/2)/128,box[2]/128,box[3]/128, (x+4)/128,(y-4)/128,8/128,8/128])
                 else:
                     scores_16.append(0)
-                    loc_16.append([0,0,0,0,0])
+                    loc_16.append([0]*9)
 
         scores_8 = []
         loc_8 = []
         for x in range(0,128,16):
-            for y in range(0,128,16):
+            for y in range(128,0,-16):
                 iou = 0
                 box = []
                 for b in boxes:
@@ -66,10 +67,10 @@ def map_anchors(path_to_annotations):
                         box = b
                 if iou > threshold:
                     scores_8.append(1)
-                    loc_8.append([1, (box[0]+box[2]/2)/128,(box[1]-box[3]/2)/128,box[2]/128,box[3]/128])
+                    loc_8.append([1,(box[0]+box[2]/2)/128,(box[1]-box[3]/2)/128,box[2]/128,box[3]/128,(x+8)/128,(y-8)/128,16/128,16/128])
                 else:
                     scores_8.append(0)
-                    loc_8.append([0,0,0,0,0]) 
+                    loc_8.append([0]*9) 
         batch_loc_8.append(loc_8)
         batch_score_8.append(scores_8)
         batch_score_16.append(scores_16)
@@ -77,6 +78,8 @@ def map_anchors(path_to_annotations):
 
     # print(np.array(batch_score_8).shape, np.array(batch_loc_8).shape)    
     # return np.array(batch_score_8), np.array(batch_loc_8), np.array(batch_score_16), np.array(batch_loc_16)
-    # print(np.concatenate((np.array(batch_score_16),np.array(batch_score_8)),axis=1).shape, np.concatenate((np.array(batch_loc_16),np.array(batch_loc_8)),axis=1).shape)
+    print(np.concatenate((np.array(batch_score_16),np.array(batch_score_8)),axis=1).shape, np.concatenate((np.array(batch_loc_16),np.array(batch_loc_8)),axis=1).shape)
 
     return np.concatenate((np.array(batch_score_16),np.array(batch_score_8)),axis=1), np.concatenate((np.array(batch_loc_16),np.array(batch_loc_8)),axis=1)
+
+map_anchors('')
